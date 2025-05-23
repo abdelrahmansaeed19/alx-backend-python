3fec6729b7b6cc2dc6962d030ef29b1b600b53bb
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-23 18:16:38 +0300

    f

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,3 +1,48 @@
+import pyodbc
+
+class DatabaseConnection:
+    def __init__(self, db_name, conn):
+        self.conn = conn
+        self.db_name = db_name
+        self.connection = None
+
+    def __enter__(self):
+        print(f"Connecting to database: {self.db_name}")
+        return self
+    
+    def connect(self):
+        try:
+            self.conn = pyodbc.connect(
+                'DRIVER={ODBC Driver 18 for SQL Server};'
+                'SERVER=localhost,1433;'
+                'DATABASE=AirBnB_Clone;'
+                'UID=sa;'
+                'PWD=20252025aS@;'
+                'Encrypt=yes;'
+                'TrustServerCertificate=yes;'
+            )
+            self.connection = True
+        except pyodbc.Error as e:
+            print(f"Error connecting to database: {e}")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print(f"An error occurred: {exc_value}")
+        self.__disconnect()
+        print("Exit method called")
+
+
+    def __disconnect(self):
+        print(f"Disconnecting from database: {self.db_name}")
+        self.conn.close()
+        self.connection = False
+
+    def execute_query(self, query):
+        if not self.connection:
+            raise Exception("Database not connected")
+        else:
+            print(f"Executing query: {query}")
+
 class ExcuteQuery:
     def __init__(self, query):
         self.query = query
@@ -13,3 +58,11 @@ class ExcuteQuery:
         print(f"Executing query: {self.query}")
         # Simulate a database operation
         return f"Result of {self.query}"
+
+with DatabaseConnection("AirBnB_Clone", None) as db:
+    db.connect()
+    db.execute_query("SELECT * FROM users WHERE age > ? 25")
+    print("Query executed successfully")
+
+    
+
