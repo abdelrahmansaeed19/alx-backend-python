d4547fcdacbfd34dcd23b2a525cb5667539a9d13
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-31 01:36:07 +0300

    f

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters  # ✅ includes 'status' and 'filters'
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework.response import Response
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created_at']
 
     def perform_create(self, serializer):
-        # Automatically add the current user as a participant when creating a conversation
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
 
@@ -20,6 +22,8 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
