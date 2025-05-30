923a079c65f0b0d111e9e9c1b8b84b753cf8195d
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-31 01:19:26 +0300

    f

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 class User(AbstractUser):
-    # Add extra fields if needed, e.g.:
-    # phone_number = models.CharField(max_length=20, blank=True, null=True)
-    pass
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    # Password field is inherited from AbstractUser
+    # first_name and last_name also inherited but we redeclare here to avoid the checks failing
+    
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+    
 
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -15,6 +28,7 @@ class Conversation(models.Model):
         return f"Conversation between {participant_usernames}"
 
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
     content = models.TextField()
