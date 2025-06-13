cbb5ee100e44aee84791822a87b05de20dd04c0f
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-06-13 13:36:29 +0300

    f

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -36,9 +36,18 @@ class Message(models.Model):
     content = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)  # ✅ New field to track edits
 
     def __str__(self):
         return f"Message from {self.sender.username} at {self.timestamp}"
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'History for Message ID {self.message.id} at {self.edited_at}'
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
